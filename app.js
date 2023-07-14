144c6ee4cc0dd4897eec0ff58a5bab486b510fee
Meghna Agrawal <meghnaagrawal10@gmail.com>
2023-07-14 14:15:58 +0530

    update blog functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,8 +16,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts = [];
+
 app.get("/", function(req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function(req, res) {
@@ -32,12 +35,24 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  let composeContent = req.body.compose;
-  console.log(composeContent);
+  let post = {
+    title: req.body.titleContent,
+    content: req.body.postContent
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
+app.get("/posts/:name", function(req, res) {
+  const requestTitle = _.lowerCase(req.params.name);
 
-
+  posts.forEach(function(post) {
+    const postTitle = _.lowerCase(post.title);
+    if(requestTitle === postTitle) {
+      res.render("post", {title: post.title, paraContent: post.content});
+    } 
+  });
+});
 
 
 
